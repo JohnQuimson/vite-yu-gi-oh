26a3ede54ae300da90430b6bb234ea201b194757
JohnQuimson <john.quimson777@gmail.com>
2023-12-12 19:04:42 +0100

    feat: structure cards

---
+++ src/components/CardElement.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'CardElement',
+  data() {
+    return {};
+  },
+  components: {},
+};
+</script>
+
+<template></template>
+
+<style lang="scss"></style>
---
+++ src/components/CardsList.vue
@@ -0,0 +1,21 @@
+<script>
+import CardElement from './CardElement.vue';
+
+export default {
+  name: 'CardsList',
+  components: {
+    CardElement,
+  },
+  data() {
+    return {
+      cards: [],
+    };
+  },
+};
+</script>
+
+<template>
+  <CardElement v-for="card in cards" />
+</template>
+
+<style lang="scss"></style>
