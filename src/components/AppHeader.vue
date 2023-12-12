caeb434f63da9cb220d60cf2d5b467c9cf4860c6
JohnQuimson <john.quimson777@gmail.com>
2023-12-12 18:37:46 +0100

    feat: add logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,7 +7,14 @@ export default {
 };
 </script>
 
-<template>ciao</template>
+<template>
+  <header>
+    <div class="cont-img">
+      <img src="../../public/img/logo.png" alt="logo" />
+    </div>
+    <h1>Yu-Gi-Oh Api</h1>
+  </header>
+</template>
 
 <style scoped lang="scss">
 @use '../assets/scss/main.scss' as *;
